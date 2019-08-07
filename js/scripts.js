d8a142f0a8a80ec37a7dc26b8bd015dd0f13235e
silvermistshadow <bmead72@gmail.com>
2019-08-07 11:36:59 -0700

    try to add an order list thing

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,42 @@
 //Back end code here
+function Order() {
+    this.pizzas = [];
+    this.currentId = 0;
+};
+
+Order.prototype.addPizza = function(pizza) {
+    pizza.id = this.assignId();
+    this.pizzas.push(pizza);
+};
+
+Order.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+};
+
+Order.prototype.findPizza = function(id) {
+    for (var i=0; i< this.pizzas.length; i++) {
+        if (this.pizzas[i]) {
+            if (this.pizzas[i].id == id) {
+                return this.pizzas[i];
+            }
+        }
+    };
+    return false;
+};
+
+Order.prototype.deletePizza = function(id) {
+    for (var i=0; i< this.pizzas.length; i++) {
+        if (this.pizzas[i]) {
+            if (this.pizzas[i].id == id) {
+                delete this.pizzas[i].id
+            }
+        }
+    };
+    return false;
+}
+
+
 function Pizza(toppings, size) {
     this.toppings = toppings;
     this.size = size;
@@ -59,8 +97,41 @@ var toppings = [];
 
 
 //UI starts here
+var order = new Order();
+
+function displayPizzaDetails(orderToDisplay) {
+    var pizzaList = $("ul#pizzas");
+    var htmlForPizzaInfo = "";
+    orderToDisplay.pizzas.forEach(function(pizza) {
+        htmlForPizzaInfo += "<li id=" + pizza.id + ">" + pizza.size + "</li>";
+    });
+    pizzaList.html(htmlForPizzaInfo);
+};
+
+function showPizza(pizzaId) {
+    var pizza = Order.findPizza(pizzaId);
+    $("#show-pizza").show();
+    $(".pizzaSize").html(pizza.size);
+    for (var i = 0; i<= pizza.toppings.length; i++) {
+        $(".pizzaToppings").append(" " + pizza.toppings[i]);
+    };
+    
+};
+
+function attachPizzaListeners() {
+    $("ul#pizzas").on("click", "li", function() {
+      showPizza(this.id);
+    });
+    $("#buttons").on("click", ".deleteButton", function() {
+      order.deletePizza(this.id);
+      $("#show-pizza").hide();
+      displayPizzaDetails(order);
+    });
+  };
+
 
 $(document).ready(function() {
+    attachPizzaListeners();
     $("form#pizzaForm").submit(function(event){
         event.preventDefault();
         $("#output").show();
@@ -71,8 +142,9 @@ $(document).ready(function() {
         var inputSize = $("#pizzaSize").val();
         var newPizza = new Pizza(toppings, inputSize);
         var outputPrice = newPizza.getPrice();
+        order.addPizza(newPizza);
         $("#output").html("Price:" + " " + outputPrice);
-
+        displayPizzaDetails(order);
     });
 
 });
